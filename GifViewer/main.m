114d2143be1c69c32bf9beae5fc1dab04ec40288
Jeff Ripke <jripke74@gmail.com>
2017-08-05 06:49:38 -0700

    initial commit

---
+++ GifViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GifViewer
+//
+//  Created by Jeff Ripke on 8/5/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
